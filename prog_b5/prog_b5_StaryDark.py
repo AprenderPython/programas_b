d498f9a3bf1c34e3cb7982534ef14ad9f757961c
AprenderPython <70590945+AprenderPython@users.noreply.github.com>
2020-10-12 17:27:55 -0400

    Create prog_b5_StaryDark.py

---
+++ prog_b5/prog_b5_StaryDark.py
@@ -0,0 +1,6 @@
+#Hecho por StaryDark     Telegram:@dark_zly
+num1 = int(input("Introduce un numero"))
+for num1 in range(1, num1 + 1):
+    print("")
+    for num2 in range(0, 13):
+        print(num1, "*", num2, "=", (num1 * num2))
